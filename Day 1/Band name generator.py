4025affe4d3abe99986c0f9dbdefbbcb2a59a863
ProphetGuru <90871530+ProphetGuru@users.noreply.github.com>
2023-05-04 21:29:08 +0100

    Add files via upload

---
+++ Day 1/Band name generator.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  4 13:29:13 2023
+
+@author: ibiaj
+"""
+
+print("Welcome to the Band Name Generator.")
+Month = input("What's the name of the month you were born?\n")
+Team = input("What's your favourite Sports Team?\n")
+print("The name of your Rock band is " + Month+ " " + Team)
