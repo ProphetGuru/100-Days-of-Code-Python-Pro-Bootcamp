47ef1cb7d8b56a4907d1acfe881492ec4a5ba1e8
ProphetGuru <90871530+ProphetGuru@users.noreply.github.com>
2023-05-07 22:49:36 +0100

    Add files via upload

---
+++ Day 3/Rollercoaster ride - if,else conditional operators.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height < 120:
+    print("Sorry mate... today`s not your day")
+else: 
+    print("Enjoy your ride")
